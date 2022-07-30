ec8446d3b3f5b5e622bc7d4f8aa5747adf02e1fa
João Pedro <joaococco@hotmail.com.br>
2022-07-30 15:13:29 -0300

    fist

---
+++ teste/newApp/views.py
@@ -0,0 +1,40 @@
+from django.urls import reverse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question
+
+# Create your views here.
+
+def index ( request ):
+
+  # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+  # context = {'latest_question_list': latest_question_list}
+  # return render(request, 'newApp/index.html', context)
+
+  lestPubliList = Question.objects.order_by('-pub_date')[:5]
+  context = {'lestPubliList' : lestPubliList}
+  return render( request , 'newApp/index.html',context)
+  #return HttpResponse ("Olá this is elon   musk")  
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results( request , question_id):
+  question = get_object_or_404(Question,  pk = question_id)
+  return render(request, 'newApp/results.html', {'quesiton': question})
+  # return HttpResponse ( 'this is result of question number %s ' %question_id  )
+
+def vote(request , question_id):
+  question = get_object_or_404(Question , pk=question_id)
+  try:
+      selected_choice = question.choice_set.get(pk=request.POST['choice'])
+  except KeyError:
+      return render(request, 'newApp/vote.html' , {
+        'question':question,
+        'error_message' : 'Yout did not select a choice ',
+      })
+  else:
+    selected_choice.votes += 1 
+    selected_choice.save()
+    return HttpResponseRedirect(reverse('newApp:results' , args=( question.id  , )))    
+  
